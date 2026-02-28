eed6ec1c9cb96a1f0d4a7df52b802f6b3ca5adc9
Keaton Nordby <keatonnordby4@gmail.com>
2026-02-28 14:18:37 -0600

    refactored routes and services, as well as resolved issues

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,59 +1,67 @@
-from fastapi import APIRouter, status
-from fastapi.exceptions import HTTPException
-from src.books.book_data import books
-from src.books.schemas import Book, BookUpdateModel
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlmodel.ext.asyncio.session import AsyncSession
 from typing import List
+import uuid
 
+from src.books.schemas import Book, BookCreateModel, BookUpdateModel
+from src.books.service import BookService
+from src.db.main import get_session
 
 
 book_router = APIRouter()
+book_service = BookService()
 
 
-
-@book_router.get('/', response_model=List[Book])
-async def get_all_books():
-    return books
-
-
-@book_router.post('/', status_code=status.HTTP_201_CREATED)
-async def create_a_book(book_data: Book) -> dict:
-    new_book = book_data.model_dump()
-    
-    books.append(new_book)
-    
-    return new_book
+@book_router.get("/", response_model=List[Book])
+async def get_all_books(session: AsyncSession = Depends(get_session)):
+    return await book_service.get_all_books(session)
 
 
-@book_router.get('/{book_id}')
-async def get_book(book_id: int) -> dict:
-    for book in books:
-        if book['id'] == book_id:
-            return book
-    
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
+async def create_a_book(
+    book_data: BookCreateModel,
+    session: AsyncSession = Depends(get_session),
+):
+    return await book_service.create_book(book_data, session)
 
 
-@book_router.patch('/{book_id}')
-async def update_book(book_id: int, book_update_data: BookUpdateModel) -> dict:
-    for book in books:
-        if book['id'] == book_id:
-            book['title'] = book_update_data.title
-            book['publisher'] = book_update_data.publisher
-            book['page_count'] = book_update_data.page_count
-            book['language'] = book_update_data.language
-            
-            return book
-    
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@book_router.get("/{book_uid}", response_model=Book)
+async def get_book(
+    book_uid: uuid.UUID,
+    session: AsyncSession = Depends(get_session),
+):
+    book = await book_service.get_book(book_uid, session)
+
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    return book
 
 
-@book_router.delete('/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_a_book(book_id: int):
-    for book in books:
-        if book['id'] == book_id:
-            books.remove(book)
-            
-            return {}
-            
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@book_router.patch("/{book_uid}", response_model=Book)
+async def update_book(
+    book_uid: uuid.UUID,
+    book_update_data: BookUpdateModel,
+    session: AsyncSession = Depends(get_session),
+):
 
+    updated_book = await book_service.update_book(book_uid, book_update_data, session)
+
+    if not updated_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    return updated_book
+
+
+@book_router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_a_book(
+    book_uid: uuid.UUID,
+    session: AsyncSession = Depends(get_session),
+):
+
+    deleted_book = await book_service.delete_book(book_uid, session)
+
+    if not deleted_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    return None
--- src/db/main.py
+++ src/db/main.py
@@ -1,22 +1,27 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
-from src.books.models import Book
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
+
 from src.config import Config
 
 
-
-
 # database connection engine
-engine = AsyncEngine(
-    create_engine(
-    url=Config.DATABASE_URL,
-    echo=True
-))
+async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 
 
 # function to keep db running while app is being used
 async def init_db():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
-        
-        
+
+
+# function to return session to route handlers
+async def get_session() -> AsyncSession:
+
+    Session = sessionmaker(
+        bind=async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with Session() as session:
+        yield session
