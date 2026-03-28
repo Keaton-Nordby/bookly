55c6fa7166dfe902eeb5c7c6d01785e264d8d67b
Keaton Nordby <keatonnordby4@gmail.com>
2026-03-28 17:23:00 -0500

    fixed access and refresh token error, user auth now up to date

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -10,28 +10,18 @@ class TokenBearer(HTTPBearer):
     def __init__(self, auto_error=True):
         super().__init__(auto_error=auto_error)
 
-    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+    async def __call__(self, request: Request):
         creds = await super().__call__(request)
 
         token = creds.credentials
-
-        token_data = decode_token(token)
-
         token_data = decode_token(token)
 
         if not self.token_valid(token):
-            
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Invalid or expired token"
             )
 
-        if token_data["refresh"]:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an access token",
-            )
-            
         self.verify_token_data(token_data)
 
         return token_data
@@ -61,5 +51,5 @@ class RefreshTokenBearer(TokenBearer):
         if token_data and not token_data["refresh"]:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an access token",
+                detail="Please provide a refresh token",
             )
